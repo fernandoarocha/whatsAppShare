74cb49f20f7c02b13d3b391dcfc5ff124e260063
Fernando Rocha <fernando.rocha@pontomobi.com.br>
2015-02-11 18:24:10 -0200

    Alteração para funcionar o share no whatsapp

--- app/src/main/java/com/what/fernandorocha/teste/MainActivity.java
+++ app/src/main/java/com/what/fernandorocha/teste/MainActivity.java
@@ -12,72 +12,139 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static android.view.View.*;
 /*
-Button one = (Button)this.findViewById(R.id.buttonChega);
-        final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.salgadin);
-        one.setOnClickListener(new View.OnClickListener(){
 
-            public void onClick(View v) {
-
-                // If the music is playing
-
-                    mp2.start();
-            }
-        });
-
-
-
-        Button two = (Button)this.findViewById(R.id.buttonTwo);
-        final MediaPlayer musictwo = MediaPlayer.create(this, R.raw.salgadin_dois);
-        two.setOnClickListener(new View.OnClickListener(){
-
-            public void onClick(View v) {
-
-                // If the music is playing
-
-                musictwo.start();
-            }
-        });
-
-        Button three = (Button)this.findViewById(R.id.buttonThree);
-        final MediaPlayer musicthree = MediaPlayer.create(this, R.raw.salgadin_tres);
-        three.setOnClickListener(new View.OnClickListener(){
-
-            public void onClick(View v) {
-
-                // If the music is playing
-
-                musicthree.start();
-            }
-        });
-
-        Button four = (Button)this.findViewById(R.id.buttonFour);
-        final MediaPlayer musicfour = MediaPlayer.create(this, R.raw.salgadin_hogod);
-        four.setOnClickListener(new View.OnClickListener(){
-
-            public void onClick(View v) {
-
-                // If the music is playing
-
-                musicfour.start();
-            }
-        });
 */
 
 
-public class MainActivity extends Activity implements OnClickListener {
-
+public class MainActivity extends Activity {
+    MusicShare musicShareOne = new MusicShare("salgadin.mp3");
+    MusicShare musicShareTwo = new MusicShare("salgadin_dois.mp3");
+    MusicShare musicShareThree = new MusicShare("salgadin_tres.mp3");
+    MusicShare musicShareFour = new MusicShare("salgadin_hogod.mp3");
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Tocar musicas clickadas
+        ImageView one = (ImageView)this.findViewById(R.id.buttonChega);
+        final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.salgadin);
+        one.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+
+                if(mp2.isPlaying())
+                    mp2.pause();// If the music is playing
+                else
+                    mp2.start();
+            }
+        });
+
+        ImageView two = (ImageView)this.findViewById(R.id.buttonTwo);
+        final MediaPlayer musictwo = MediaPlayer.create(this, R.raw.salgadin_dois);
+        two.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+
+                if(musictwo.isPlaying())
+                    musictwo.pause();// If the music is playing
+                else
+                    musictwo.start();
+            }
+        });
+
+        ImageView three = (ImageView)this.findViewById(R.id.buttonThree);
+        final MediaPlayer musicthree = MediaPlayer.create(this, R.raw.salgadin_tres);
+        three.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+
+                if(musicthree.isPlaying())
+                    musicthree.pause();// If the music is playing
+                else
+                    musicthree.start();
+            }
+        });
+
+        ImageView four = (ImageView)this.findViewById(R.id.buttonFour);
+        final MediaPlayer musicfour = MediaPlayer.create(this, R.raw.salgadin_hogod);
+        four.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+
+                if(musicfour.isPlaying())
+                    musicfour.pause();// If the music is playing
+                else
+                    musicfour.start();
+            }
+        });
+
+        ImageView shareOne = (ImageView)this.findViewById(R.id.buttonShareOne);
+
+        shareOne.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = ShareCompat.IntentBuilder.from(MainActivity.this)
+                        .setType("audio/*")
+                        .setStream(musicShareOne.mImageUri)
+                        .setChooserTitle("Compartilhar Áudio com")
+                        .createChooserIntent();
+                startActivity(shareIntent);
+            }
+        });
+
+        ImageView shareTwo = (ImageView)this.findViewById(R.id.buttonShareTwo);
+
+        shareTwo.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = ShareCompat.IntentBuilder.from(MainActivity.this)
+                        .setType("audio/*")
+                        .setStream(musicShareTwo.mImageUri)
+                        .setChooserTitle("Compartilhar Áudio com")
+                        .createChooserIntent();
+                startActivity(shareIntent);
+            }
+        });
+
+        ImageView shareThree = (ImageView)this.findViewById(R.id.buttonShareThree);
+
+        shareThree.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = ShareCompat.IntentBuilder.from(MainActivity.this)
+                        .setType("audio/*")
+                        .setStream(musicShareThree.mImageUri)
+                        .setChooserTitle("Compartilhar Áudio com")
+                        .createChooserIntent();
+                startActivity(shareIntent);
+            }
+        });
+
+
+        ImageView shareFour = (ImageView)this.findViewById(R.id.buttonShareFour);
+
+        shareFour.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = ShareCompat.IntentBuilder.from(MainActivity.this)
+                        .setType("audio/*")
+                        .setStream(musicShareFour.mImageUri)
+                        .setChooserTitle("Compartilhar Áudio com")
+                        .createChooserIntent();
+                startActivity(shareIntent);
+            }
+        });
 
     }
 
@@ -105,18 +172,6 @@ public class MainActivity extends Activity implements OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onClick(View v) {
-
-
-        Intent shareIntent = ShareCompat.IntentBuilder.from(MainActivity.this)
-                .setText("Beatifull")
-                .setType("audio/*")
-                .setStream(MusicShare.)
-                .setChooserTitle("Imagem")
-                .createChooserIntent();
-        startActivity(shareIntent);
-    }
 
     private static class MusicShare{
         private final Uri mImageUri;
